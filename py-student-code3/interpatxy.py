eb3d65944485e24af50bb71433adeafbc62120fe
David G. Turner <turnerdavid330@gmail.com>
2025-11-01 13:44:54 -0400

    style: fixed a bunch of flake8 issues

--- py-student-code3/interpatxy.py
+++ py-student-code3/interpatxy.py
@@ -2,16 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from bspline2b import bspline2b
 
+
 def interpatxy(x: np.ndarray, y: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """
     This version uses the natural end condition
-    Uses Matlab \ to solve linear systems
+    Uses Matlab \\ to solve linear systems
     Input points: two column vectors of x and y coordinates of dim N+1
-    
+
     This version uses x_0, x_1, ..., x_{N-1}, x_N to compute the Bezier
     points and the subdivision version of the de Casteljau algorithm
     to plot the Bezier segments (bspline2b and drawbezier_dc)
-    
+
     This version outputs the x and y coordinates dx and dy of the de Boor control
     points d_{-1}, d_0, d_1, ..., d_{N+1} as column vectors
     and the x and y coordinates of the Bezier control polygons
@@ -28,9 +29,9 @@ def interpatxy(x: np.ndarray, y: np.ndarray) -> tuple[np.ndarray, np.ndarray, np
     # Plots the spline
     Nx = len(dx)-1
     print(f"Nx = {Nx}")
-    nn = 6 # Subdivision level
+    nn = 6  # Subdivision level
     drawb = True
-    Bx, By = bspline2b(dx,dy,Nx,nn,drawb) # Copy your version of bspline2b into bspline2b.py
+    Bx, By = bspline2b(dx, dy, Nx, nn, drawb)  # Copy your version of bspline2b into bspline2b.py
     plt.ion()
     plt.plot(x, y, 'b+')
     plt.ioff()
